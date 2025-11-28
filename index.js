e2e62e4a27d91c5c0fc6a490e346a722dacd2425
thatamazighii <karroumelmessaoudi52@gmail.com>
2025-11-28 18:37:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-//Make sure
 import express from "express";
 import fs from "fs";
 
 const app = express();
 app.use(express.text({ type: "*/*" }));
 
-app.post("/", (req, res) => {
-    fs.writeFileSync("data.txt", req.body);
+app.post("/:username", (req, res) => {
+    const file = `data_${req.params.username}.txt`;
+    fs.writeFileSync(file, req.body);
     res.send("OK");
 });
 
-app.get("/", (req, res) => {
-    if (!fs.existsSync("data.txt")) return res.send("");
+app.get("/:username", (req, res) => {
+    const file = `data_${req.params.username}.txt`;
+    if (!fs.existsSync(file)) return res.send("");
     res.type("text/plain");
-    res.send(fs.readFileSync("data.txt", "utf8"));
+    res.send(fs.readFileSync(file, "utf8"));
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log("Server running on " + port));
+app.listen(port);
